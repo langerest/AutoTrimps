166b4ce704bb4327450d0d75725741a39f1034da
Kaili Jiang <7516195+langerest@users.noreply.github.com>
2023-10-21 16:42:09 -0400

    Revert changes for bug fix

--- modules/breedtimer.js
+++ modules/breedtimer.js
@@ -93,7 +93,6 @@ function breedTimeRemaining() {
 var DecimalBreed = Decimal.clone({precision: 30, rounding: 4});
 var missingTrimps = new DecimalBreed(0);
 function ATGA2() {
-    debug("ATGA2")
 	if (game.jobs.Geneticist.locked == false && getPageSetting('ATGA2') == true && getPageSetting('ATGA2timer') > 0 && game.global.challengeActive != "Trapper"){
 		var trimps = game.resources.trimps;
 		var trimpsMax = trimps.realMax();
@@ -172,7 +171,7 @@ function ATGA2() {
 		var genDif = new DecimalBreed(Decimal.log10(target.div(compareTime)).div(Decimal.log10(1.02))).ceil();
         debug("CompareTime:" + compareTime +" breedTime:" + breedtime + " timeRemaining:" + timeRemaining + " totalTime:" + totalTime);
 
-        if (compareTime.add(breedtime).cmp(target) < 0 || (compareTime.cmp(target) < 0 && timeRemaining.cmp(1) <= 0)) {
+        if (compareTime.cmp(target) < 0) {
 			if (game.resources.food.owned * (getPageSetting('ATGA2gen')/100) < getNextGeneticistCost()) {return;}
 			else if (timeRemaining.cmp(1) < 0 || target.minus(breedtime).cmp(timeRemaining) > 0){
 				if (genDif.cmp(0) > 0){
@@ -181,8 +180,7 @@ function ATGA2() {
 				}
 			}
 		}
-		else if ((compareTime.add(thresh.mul(-1)).cmp(target.minus(breedtime)) > 0 && timeRemaining.cmp(1) > 0)
-             || compareTime.add(thresh.mul(-1)).cmp(target) > 0  || (potencyMod.cmp(1) == 0)){
+		else if (compareTime.add(thresh.mul(-1)).cmp(target) > 0  || (potencyMod.cmp(1) == 0)){
 			if (!genDif.isFinite()) genDif = new Decimal(-1);
 			if (genDif.cmp(0) < 0 && game.options.menu.gaFire.enabled != 2){
 				if (genDif.cmp(-10) < 0) genDif = new Decimal(-10);
