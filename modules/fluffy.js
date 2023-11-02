818213c58e00156d20253cd9c100884807af64c7
Kaili Jiang <7516195+langerest@users.noreply.github.com>
2023-11-02 02:13:59 -0400

    Bug fix

--- modules/fluffy.js
+++ modules/fluffy.js
@@ -3,7 +3,7 @@ function EvolveFluffy()
     if (Fluffy.currentLevel < 10) return;
     if (game.global.challengeActive == "Daily" && !getPageSetting("FluffyEvolveDaily")) return;
     if (game.global.world >= getPageSetting("MinFluffyEvolveZ") && (getPageSetting("MaxFluffyEvolveZ") == -1 || game.global.world < getPageSetting("MaxFluffyEvolveZ"))) {
-        prestige();
+        Fluffy.prestige();
         debug("Evolve Fluffy to Evolve " + Fluffy.getCurrentPrestige());
     }
 }
